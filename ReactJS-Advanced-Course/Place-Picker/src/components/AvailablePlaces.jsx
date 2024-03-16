48c91a73cbef75f8a83a291a3b2e56eabfeb2774
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-16 11:59:35 +0330

    Using "async await" for Fetching Data

--- ReactJS-Advanced-Course/Place-Picker/src/components/AvailablePlaces.jsx
+++ ReactJS-Advanced-Course/Place-Picker/src/components/AvailablePlaces.jsx
@@ -6,13 +6,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   // to prevent infinite loop
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
@@ -24,3 +24,34 @@ export default function AvailablePlaces({ onSelectPlace }) {
     />
   );
 }
+
+/* fetch()
+fetch('http://localhost:3000/places')  // return a Promise
+  .then((response) => {        // response: received from resolving Promise
+                    /// yield different values & to access those values
+                      we chain methods on the result of calling fetch
+
+                         /// parsing the data received in JSON format
+      return response.json();  // extract Data that's attached 
+    })                      in the JSON format- text-based data format
+                                - JSON return another Promise
+
+  .then((resData) => {      // get back response Data & work with Data
+      setAvailablePlaces(resData.places);
+   });
+   
+  ? Promise:
+   - it's a wrapper object around a value - here is a wrapper around response obj 
+
+  ? JSON():
+   - The response.json() method is used to extract the JSON body content from a Response object returned by a fetch request. 
+   - It parses the response body as JSON and returns a promise that resolves with the result of parsing the body text as JSON.
+
+! this code can create an infinite loop - calling fetch directly in component
+   - when component gets executed
+   - a new request would be sent
+   - then state gets updated 
+   - which causes the component to execute again
+   - then again we end up with new request, new update and so on ...
+
+*/
