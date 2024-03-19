e438674548cf94e030301f20ed739dc8b595935d
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-19 16:22:01 +0330

    🔧 Refactor Codes in NewTask Component

--- ReactJS-Advanced-Course/SimpleTaskApp-CustomHooks/src/components/NewTask/NewTask.js
+++ ReactJS-Advanced-Course/SimpleTaskApp-CustomHooks/src/components/NewTask/NewTask.js
@@ -5,8 +5,53 @@ import { useInput } from '../../hooks/useInput';
 export default function NewTask({ onAddTask }) {
   const { isLoading, error, sendRequest: sendTaskRequest } = useInput();
 
-  const createTask = (taskData, taskText) => {
-    // firebase-specific => "name" contains generated id
+  const enterTaskHandler = async (taskText) => {
+    const createTask = (taskData) => {
+      // firebase-specific => "name" contains generated id
+      const generatedId = taskData.name;
+      const createdTask = { id: generatedId, text: taskText };
+
+      onAddTask(createdTask);
+    };
+
+    sendTaskRequest(
+      {
+        url: 'https://react-custom-hook-f29d8-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      createTask
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+
+      {error && <p> {error.message} </p>}
+    </Section>
+  );
+}
+
+/*  createTask.bind(null, taskText)
+----------------------------
+- in our custom hook (useInput)
+- the nested fn(sendRequest)
+- has an argument(applyData)
+- which only gets one argument(data)
+
+* now if we want our fn(createTask) to have access to both arguments from useInput and applyData, we can bind them together like this
+
+- null : set "this" keyword in the to-be-executed fn(createTask)
+
+export default function NewTask({ onAddTask }) {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useInput();
+
+?  const createTask = (taskData, taskText) => {
+    /// firebase-specific => "name" contains generated id
     const generatedId = taskData.name;
     const createdTask = { id: generatedId, text: taskText };
 
@@ -23,7 +68,7 @@ export default function NewTask({ onAddTask }) {
           'Content-Type': 'application/json',
         },
       },
-      createTask.bind(null, taskText),
+?      createTask.bind(null, taskText)
     );
   };
 
@@ -31,21 +76,8 @@ export default function NewTask({ onAddTask }) {
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
 
-      {error && <p> {error} </p>}
+      {error && <p> {error.message} </p>}
     </Section>
   );
 }
-
-/*  createTask.bind(null, taskText)
-----------------------------
-- in our custom hook (useInput)
-- the nested fn(sendRequest)
-- has an argument(applyData)
-- which only gets one argument(data)
-
-* now if we want our fn(createTask) to have access to both arguments from useInput and applyData, we can bind them together like this
-
-- null : set "this" keyword in the to-be-executed fn(createTask)
- 
-
 */
