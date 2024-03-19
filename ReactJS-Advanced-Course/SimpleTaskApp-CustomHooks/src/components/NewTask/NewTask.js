5f71057a144c0bcc5ac8b8710a9d5b3a0adfcc2d
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-19 15:45:09 +0330

    🖥️ Using The Custom Hook In More Components - NewTask Component

--- ReactJS-Advanced-Course/SimpleTaskApp-CustomHooks/src/components/NewTask/NewTask.js
+++ ReactJS-Advanced-Course/SimpleTaskApp-CustomHooks/src/components/NewTask/NewTask.js
@@ -1,43 +1,30 @@
-import { useState } from 'react';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import { useInput } from '../../hooks/useInput';
 
 export default function NewTask({ onAddTask }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useInput();
+
+  const createTask = (taskData, taskText) => {
+    // firebase-specific => "name" contains generated id
+    const generatedId = taskData.name;
+    const createdTask = { id: generatedId, text: taskText };
+
+    onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(
-        'https://react-custom-hook-f29d8-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request Failed!');
-      }
-
-      const data = await response.json();
-
-      // firebase-specific => "name" contains generated id
-      const generatedId = data.name;
-      const createdTask = { id: generatedId, text: taskText };
-
-      onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something Went Wrong!');
-    }
-
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: 'https://react-custom-hook-f29d8-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      createTask.bind(null, taskText),
+    );
   };
 
   return (
@@ -48,3 +35,17 @@ export default function NewTask({ onAddTask }) {
     </Section>
   );
 }
+
+/*  createTask.bind(null, taskText)
+----------------------------
+- in our custom hook (useInput)
+- the nested fn(sendRequest)
+- has an argument(applyData)
+- which only gets one argument(data)
+
+* now if we want our fn(createTask) to have access to both arguments from useInput and applyData, we can bind them together like this
+
+- null : set "this" keyword in the to-be-executed fn(createTask)
+ 
+
+*/
