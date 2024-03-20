2dd539763398bdfe45faced58415efacf0b0ceab
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-20 11:41:09 +0330

    ⌨️ Getting User Input via State | Two-way binding

--- ReactJS-Advanced-Course/Login-Form/src/components/Login.jsx
+++ ReactJS-Advanced-Course/Login-Form/src/components/Login.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(enteredValues);
+  };
+
+  const handleInputChange = (identifier, value) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   };
 
   return (
@@ -10,12 +25,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email"> Email </label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(e) => handleInputChange('email', e.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password"> Password </label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(e) => handleInputChange('password', e.target.value)}
+          />
         </div>
       </div>
 
@@ -61,4 +88,58 @@ export default function Login() {
            const handleSubmit = (e) => {
               e.preventDefault();
             };
+*/
+
+/* Handling Value with "Multiple-state"
+export default function Login() {
+*  const [enteredEmail, setEnteredEmail] = useState('');
+*  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+*  const handleEmailChange = (e) => {
+*    setEnteredEmail(e.target.value);
+  };
+
+*  const handlePasswordChange = (e) => {
+*    setEnteredPassword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2> Login </h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email"> Email </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+*            value={enteredEmail}
+*            onChange={handleEmailChange}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password"> Password </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+*            value={enteredPassword}
+*            onChange={handlePasswordChange}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat"> Reset </button>
+        <button className="button"> Login </button>
+      </p>
+    </form>
+  );
+}
 */
