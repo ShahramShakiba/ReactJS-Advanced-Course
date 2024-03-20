c1e9250061825b51c0a497dc7c71ee373c15b273
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-20 21:10:42 +0330

    Introducing Third-Party Form Libraries

--- ReactJS-Advanced-Course/Login-Form/src/App.jsx
+++ ReactJS-Advanced-Course/Login-Form/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx';
-import Signup from './components/Signup.jsx';
+// import Signup from './components/Signup.jsx';
 import StateLogin from './components/StateLogin.jsx';
 
 export default function App() {
@@ -14,3 +14,9 @@ export default function App() {
     </>
   );
 }
+
+/* React Form Library
+ *  - https://react-hook-form.com/
+
+ *  - https://formik.org/
+ */
