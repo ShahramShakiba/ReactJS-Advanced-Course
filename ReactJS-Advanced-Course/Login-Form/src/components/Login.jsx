19a6b839fd73ea632c0003757e1b0556c4c24ff9
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-20 10:40:37 +0330

    Handling Form Submission

--- ReactJS-Advanced-Course/Login-Form/src/components/Login.jsx
+++ ReactJS-Advanced-Course/Login-Form/src/components/Login.jsx
@@ -1,7 +1,11 @@
 export default function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
+      <h2> Login </h2>
 
       <div className="control-row">
         <div className="control no-margin">
@@ -22,3 +26,39 @@ export default function Login() {
     </form>
   );
 }
+
+/* Button inside of a Form element
+- the default browser behavior for BTNs in form is that those BTNs submit the form 
+- and technically that means that an HTTP Request is created 
+- and is send to the server that's serving the website
+
+? - Buttons in form will generate requests and send those req to the server which this default behavior is a "PROBLEM" 
+
+! How we can prevent the default behavior :
+*  01. add type  →  type="button"  → will not submit the form because
+       the default type is the "submit" type
+    
+    <button type="button" className="button" onClick={handleSubmit}>
+          Login
+    </button>
+
+
+*  02. the elegant way → remove type from the btn
+                        → remove onClick Listener
+        <button className="button">
+          Login
+        </button>
+                        → add onSubmit event handler prop on the "form"
+        <form onSubmit={handleSubmit}>
+          ....
+        </form>
+        
+        - which trigger a submit event which you can listen and react whenever it's submitted by one of its BTNs being pressed
+
+        - then we trigger "handleSubmit" fn here
+        - now automatically we get an event object
+        - and we pass that event-obj to the fn
+           const handleSubmit = (e) => {
+              e.preventDefault();
+            };
+*/
