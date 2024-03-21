1506008e6decd78fe5afc6f4e48e7a7924a7955a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-21 11:56:07 +0330

    Getting User Input Values

--- ReactJS-Advanced-Course/User-Input/src/components/SimpleInput.js
+++ ReactJS-Advanced-Course/User-Input/src/components/SimpleInput.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+// import { useRef } from 'react';
+
 export default function SimpleInput() {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  // const invalidInput = enteredValue === '';
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // if (invalidInput) {
+    //   return;
+    // }
+
+    console.log(enteredValue);
+    setEnteredValue('');
+  };
+
+  const handleChange = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-control">
         <label htmlFor="name"> Your Name </label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          value={enteredValue}
+        />
+
+        {/* {invalidInput && (
+          <div>
+            <p className="error-text">Please enter your name.</p>
+          </div>
+        )} */}
       </div>
 
       <div className="form-actions">
@@ -12,3 +44,29 @@ export default function SimpleInput() {
     </form>
   );
 }
+
+/* useRef
+export default function SimpleInput() {
+  const nameInput = useRef('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const enteredName = nameInput.current.value;
+    console.log(enteredName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label htmlFor="name"> Your Name </label>
+        <input type="text" id="name" ref={nameInput} />
+      </div>
+
+      <div className="form-actions">
+        <button> Submit </button>
+      </div>
+    </form>
+  );
+}
+*/
