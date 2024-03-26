2b59da25268c6ea6b4e732da9854c9ac22a198c8
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-26 13:01:48 +0330

    Adding Form Validation

--- ReactJS-Advanced-Course/Ordering-Food/src/components/Cart/Checkout.js
+++ ReactJS-Advanced-Course/Ordering-Food/src/components/Cart/Checkout.js
@@ -1,7 +1,17 @@
 import classes from './Checkout.module.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 0;
+const isElevenChars = (value) => value.trim().length !== 11;
 
 export default function Checkout({ onCancel }) {
+  const [inputValidity, setInputValidity] = useState({
+    name: true,
+    street: true,
+    postal: true,
+    phone: true,
+  });
   const name = useRef();
   const street = useRef();
   const postal = useRef();
@@ -14,28 +24,83 @@ export default function Checkout({ onCancel }) {
     const enteredStreet = street.current.value;
     const enteredPostal = postal.current.value;
     const enteredPhone = phone.current.value;
+
+    const validEnteredName = !isEmpty(enteredName);
+    const validEnteredStreet = !isEmpty(enteredStreet);
+    const validEnteredPostal = !isFiveChars(enteredPostal);
+    const validEnteredPhone = !isElevenChars(enteredPhone);
+
+    setInputValidity({
+      name: validEnteredName,
+      street: validEnteredStreet,
+      postal: validEnteredPostal,
+      phone: validEnteredPhone,
+    });
+
+    const validForm =
+      validEnteredName &&
+      validEnteredStreet &&
+      validEnteredPostal &&
+      validEnteredPhone;
+
+    if (!validForm) {
+      return;
+    }
   };
+
+  const nameCSS = `${classes.control} ${
+    !inputValidity.name && classes.invalid
+  }`;
+  const streetCSS = `${classes.control} ${
+    !inputValidity.street && classes.invalid
+  }`;
+  const postalCSS = `${classes.control} ${
+    !inputValidity.postal && classes.invalid
+  }`;
+  const phoneCSS = `${classes.control} ${
+    !inputValidity.phone && classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={handleConfirm}>
-      <div className={classes.control}>
+      <div className={nameCSS}>
         <label htmlFor="name"> Your Name </label>
         <input type="text" name="name" id="name" ref={name} />
+
+        {!inputValidity.name && (
+          <p className={classes.errorMsg}> Please Enter A Valid Name. </p>
+        )}
       </div>
 
-      <div className={classes.control}>
+      <div className={streetCSS}>
         <label htmlFor="street"> Street </label>
         <input type="text" name="street" id="street" ref={street} />
+
+        {!inputValidity.street && (
+          <p className={classes.errorMsg}> Please Enter A Valid Street. </p>
+        )}
       </div>
 
-      <div className={classes.control}>
+      <div className={postalCSS}>
         <label htmlFor="postal"> Postal Code </label>
         <input type="text" name="postal" id="postal" ref={postal} />
+
+        {!inputValidity.postal && (
+          <p className={classes.errorMsg}>
+            Please Enter A Valid Postal Code(5 characters long).
+          </p>
+        )}
       </div>
 
-      <div className={classes.control}>
+      <div className={phoneCSS}>
         <label htmlFor="phone"> Phone </label>
         <input type="number" name="phone" id="phone" ref={phone} />
+
+        {!inputValidity.phone && (
+          <p className={classes.errorMsg}>
+            Please Enter A Valid Phone Number(11 digits).
+          </p>
+        )}
       </div>
 
       <div className={classes.actions}>
