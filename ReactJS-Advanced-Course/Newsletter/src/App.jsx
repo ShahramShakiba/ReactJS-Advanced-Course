b7faff7df60cb9b0c38a58b6526b96de856999ae
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-30 12:06:07 +0330

    An alternative way of defining routes with "JSX Codes"

--- ReactJS-Advanced-Course/Newsletter/src/App.jsx
+++ ReactJS-Advanced-Course/Newsletter/src/App.jsx
@@ -1,12 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import HomePage from './pages/Home';
+import Products from './pages/Products';
+
+//defining routes in "an array of objects"
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <Products /> },
+]);
 
 export default function App() {
-  //pass an array of route-definition objects
-  const router = createBrowserRouter([
-    { path: '/', element: <HomePage /> },
-    { path: '', element: '' },
-  ]);
-
   return <RouterProvider router={router} />;
 }
+
+/* defining routes with "JSX Codes" 
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
+*/
---
+++ ReactJS-Advanced-Course/Newsletter/src/pages/Products.jsx
@@ -0,0 +1,3 @@
+export default function Products() {
+  return <h1>The Products Page!</h1>;
+}
