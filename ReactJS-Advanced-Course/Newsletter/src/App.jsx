5295a5b0a86f64850844ea44cd5fc594ff5677f0
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-30 11:36:41 +0330

    Defining First Route with createBrowserRouter & RouterProvider

--- ReactJS-Advanced-Course/Newsletter/src/App.jsx
+++ ReactJS-Advanced-Course/Newsletter/src/App.jsx
@@ -1,3 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+
 export default function App() {
-  return <></>;
+  //pass an array of route-definition objects
+  const router = createBrowserRouter([
+    { path: '/', element: <HomePage /> },
+    { path: '', element: '' },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
---
+++ ReactJS-Advanced-Course/Newsletter/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>My Home Page!</h1>;
+}
