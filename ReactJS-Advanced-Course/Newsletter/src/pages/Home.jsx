e64190742c8ee910774eb8690a5b5799400dbce2
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-30 12:40:29 +0330

    Navigating between Pages with "<Link><Link />"

--- ReactJS-Advanced-Course/Newsletter/src/pages/Home.jsx
+++ ReactJS-Advanced-Course/Newsletter/src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
-  return <h1>My Home Page!</h1>;
+  return (
+    <>
+      <h1> My Home Page! </h1>
+      <p>
+        Go to <Link to="/products">the list of Products</Link>.
+      </p>
+    </>
+  );
 }
+
+/* What does "Link" do?
+* It allows you to create hyperlinks to other routes in your application without causing a "full page reload". 
+
+* When a user clicks on a Link component, React Router intercepts(stop) the click-event and handles the navigation "internally", updating the URL and rendering the appropriate component associated with the target route.
+*/
