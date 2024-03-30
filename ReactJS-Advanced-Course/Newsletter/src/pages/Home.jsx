a777814bbe76442d3832e0209a73fcc85ce162da
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-30 15:17:19 +0330

    Navigating Programmatically via useNavigate()

--- ReactJS-Advanced-Course/Newsletter/src/pages/Home.jsx
+++ ReactJS-Advanced-Course/Newsletter/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  };
+
   return (
     <>
       <h1> My Home Page! </h1>
       <p>
         Go to <Link to="/products">the list of Products</Link>.
+      </p>
+
+      <p>
+        <button onClick={navigateHandler}> Navigating Programmatically </button>
       </p>
     </>
   );
