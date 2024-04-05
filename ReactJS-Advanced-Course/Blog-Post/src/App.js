0929b5a5080c42ef342e92561d58eb121d13eb6a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-05 22:34:58 +0330

    Update Some Explanation

--- ReactJS-Advanced-Course/Blog-Post/src/App.js
+++ ReactJS-Advanced-Course/Blog-Post/src/App.js
@@ -6,7 +6,7 @@ import { lazy, Suspense } from 'react';
 import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
 
-// Load Blog-Page & Post-Page lazily
+// implement ` Lazy Loading ` 
 const BlogPage = lazy(() => import('./pages/Blog'));
 const PostPage = lazy(() => import('./pages/Post'));
 
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
               //import: yield a Promise || module = loaded-file
               import('./pages/Blog').then((module) => module.loader()),
           },
-          
+
           {
             path: ':id',
             element: (
@@ -72,4 +72,35 @@ loader: async ({ params }) => {
 ?- here we need to pass params to the loader 
 * - we can extract params like this {params} and pass it to loader({params})
 * - or we simply take that overall meta-object 
+*/
+
+/* Deploying on "Firebase"
+Go to Firebase site
+create a new project
+build
+Hosting
+get started
+
+Install Firebase CLI: npm install -g firebase-tools
+
+Install Sign in to Google: firebase login
+You might get an error, to fix it: 
+
+1. bypass the script execution policy temporarily: Set-ExecutionPolicy Unrestricted -Scope Process
+2. install: firebase login
+3. install: firebase init
+4. Choose: Hosting: Configure files for Firebase Hosting and (optionally) set up GitHub 
+Action deploys ---- then click "space" then hit "enter"
+5. choose:  Use an existing project
+6. choose the project you created
+7. What do you want to use as your public directory? build
+8.  Configure as a single-page app (rewrite all urls to /index.html)? (y/N) y 
+9.  Set up automatic builds and deploys with GitHub? (y/N) n
+10. File build/index.html already exists. Overwrite? (y/N) n
+11. install: firebase deploy
+
+At the end
+12. set the execution policy back to its original: Set-ExecutionPolicy Restricted -Scope Process
+
+to disable your site: install: firebase hosting:disable
 */
