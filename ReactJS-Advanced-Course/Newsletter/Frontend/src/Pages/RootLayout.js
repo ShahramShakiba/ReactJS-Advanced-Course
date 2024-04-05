1ba11b966831ed75789102c3199b0939aa1bae85
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-05 10:56:39 +0330

    Adding Automatic Logout

--- ReactJS-Advanced-Course/Newsletter/Frontend/src/Pages/RootLayout.js
+++ ReactJS-Advanced-Course/Newsletter/Frontend/src/Pages/RootLayout.js
@@ -1,8 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 export default function RootLayoutPage() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  // automatically logout after 1H
+  useEffect(() => {
+    if (!token) return;
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'POST' });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
