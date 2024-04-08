07404e9c0dbdaab391db4882fd4a7c84af4c85c5
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-08 14:00:28 +0330

    Optimistic Updating for Editing Events

--- ReactJS-Advanced-Course/React-Events/src/components/Events/EditEvent.jsx
+++ ReactJS-Advanced-Course/React-Events/src/components/Events/EditEvent.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
-import { fetchEvent, updateEvent } from '../util/http.js';
+import { fetchEvent, queryClient, updateEvent } from '../util/http.js';
 import LoadingIndicator from './../UI/LoadingIndicator';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import EventForm from './EventForm.jsx';
@@ -18,6 +18,30 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+
+    // Optimistic Updating 👇 
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      //cancelling ongoing queries | it won't cancel mutation
+      await queryClient.cancelQueries({ queryKey: ['events', id] });
+      //roll-back our optimistic update if it does "fail" on the backend
+      const previousEvent = queryClient.getQueryData(['events', id]);
+
+      //"queryClient": update data behind the scenes and interact with the React-Query
+      queryClient.setQueryData(['events', id], newEvent);
+
+      return { previousEvent };
+    },
+
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', id], context.previousEvent);
+    },
+
+    // fetch the latest-data
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', id]);
+    },
   });
 
   function handleSubmit(formData) {
@@ -71,3 +95,35 @@ export default function EditEvent() {
 
   return <Modal onClose={handleClose}> {content} </Modal>;
 }
+
+/* onMutate
+- will be execute right when you call mutate, before this process is done, before you got back a response
+*/
+
+/* .cancelQueries({ queryKey: ['events', id] });
+
+* making sure if we had any outgoing queries for that key, those queries would be canceled and we would not have clashing response data from those queries and our optimistically updated query data
+
+* - because if those ongoing queries finished before the updating request was done, we would've fetched old data again
+
+* - this will returns a "Promise" and in oder to use "await" here we should add async 
+*/
+
+/* setQueryData(['events', id], newEvent);
+    - first argument: the key of that query that u do wanna edit
+    - second argument: what u want to replace it with, that new-data u wanna store
+        -- which is that data u sent to the backend through handleSubmit
+        -- react-query passes this data to => onMutate: (data) => {} as a value
+
+* to manipulate that already stored data without waiting for a response
+
+*/
+
+/* queryClient.getQueryData(); 
+
+- currently stored query-data, which we wanna execute before we set it to some new-data
+*/
+
+/* onSettled
+- will be called whenever this mutation is done, no matter if it failed or succeeded
+*/
