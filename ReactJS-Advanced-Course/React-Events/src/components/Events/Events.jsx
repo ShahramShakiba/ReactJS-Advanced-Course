c8f56427314bacee8a5d0e405345e8036e43d247
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-08 22:49:12 +0330

    Update UI

--- ReactJS-Advanced-Course/React-Events/src/components/Events/Events.jsx
+++ ReactJS-Advanced-Course/React-Events/src/components/Events/Events.jsx
@@ -1,9 +1,9 @@
 import { Link, Outlet } from 'react-router-dom';
 
-import Header from '../Header.jsx';
 import EventsIntroSection from './EventsIntroSection.jsx';
 import FindEventSection from './FindEventSection.jsx';
 import NewEventsSection from './NewEventsSection.jsx';
+import Header from '../Header.jsx';
 
 export default function Events() {
   return (
