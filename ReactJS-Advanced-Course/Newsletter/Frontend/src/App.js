9e2e46d66c8b2e262a1f85cd2dd72fd78878a410
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-09 12:51:01 +0330

    Updating the UI Based on Auth Status

--- ReactJS-Advanced-Course/Newsletter/Frontend/src/App.js
+++ ReactJS-Advanced-Course/Newsletter/Frontend/src/App.js
@@ -17,12 +17,15 @@ import AuthenticationPage, {
   action as authAction,
 } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    loader: tokenLoader,
+    id: 'root',
     children: [
       { index: true, element: <HomePage /> },
       {
--- ReactJS-Advanced-Course/Newsletter/Frontend/src/components/EventItem.js
+++ ReactJS-Advanced-Course/Newsletter/Frontend/src/components/EventItem.js
@@ -1,8 +1,9 @@
-import { Link, useSubmit } from 'react-router-dom';
+import { Link, useRouteLoaderData, useSubmit } from 'react-router-dom';
 
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
+  const token = useRouteLoaderData('root');
   const submit = useSubmit();
 
   function startDeleteHandler() {
@@ -16,13 +17,16 @@ function EventItem({ event }) {
   return (
     <article className={classes.event}>
       <img src={event.image} alt={event.title} />
-      <h1>{event.title}</h1>
-      <time>{event.date}</time>
-      <p>{event.description}</p>
-      <menu className={classes.actions}>
-        <Link to="edit">Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
-      </menu>
+      <h1> {event.title} </h1>
+      <time> {event.date} </time>
+      <p> {event.description} </p>
+
+      {token && (
+        <menu className={classes.actions}>
+          <Link to="edit">Edit</Link>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </menu>
+      )}
     </article>
   );
 }
